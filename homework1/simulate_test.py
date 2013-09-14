c9f5a34c97cff2b0d206dc2bd286b414d424cf72
Benjamin Ross <benr22@gmail.com>
2013-09-14 08:42:59 -0700

    Matching the output given, need to make loops for figuring out allocations

--- homework1/simulate_test.py
+++ homework1/simulate_test.py
@@ -8,6 +8,22 @@ dt_end = dt.datetime(2011, 12, 31)
 
 vol, daily_ret, sharpe, cum_ret = simulate(dt_start, dt_end, symbols, allocations)
 
+print "Portfolio Symbols: " + str(symbols)
+print "Portfolio Allocations: " + str(allocations)
+print "Volatility (stdev of daily returns): " + str(vol)
+print "Average Daily Return: " + str(daily_ret)
+print "Sharpe Ratio: " + str(sharpe)
+print "Cumulative Return " + str(cum_ret)
+
+symbols = ['AXP','HPQ','IBM','HNZ']
+allocations = [0.0,0.0,0.0,1.0]
+dt_start = dt.datetime(2010, 1, 1)
+dt_end = dt.datetime(2010, 12, 31)
+
+vol, daily_ret, sharpe, cum_ret = simulate(dt_start, dt_end, symbols, allocations)
+
+print "Portfolio Symbols: " + str(symbols)
+print "Portfolio Allocations: " + str(allocations)
 print "Volatility (stdev of daily returns): " + str(vol)
 print "Average Daily Return: " + str(daily_ret)
 print "Sharpe Ratio: " + str(sharpe)
